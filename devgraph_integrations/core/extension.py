843078b781b130c5ab692c92298b38a354eb4536
Craig Tracey <craig@arctir.com>
2025-11-25 13:18:59 +0000

    fix: change the stevedore molecules namespace
    Signed-off-by: Craig Tracey <craig@arctir.com>

--- devgraph_integrations/core/extension.py
+++ devgraph_integrations/core/extension.py
@@ -6,8 +6,8 @@ from devgraph_integrations.core.provider import Provider
 
 class DiscoveryExtensionManager:
     def __init__(self):
-        self.pm = PluginManager("devgraph.discovery.molecules")
-        self.pm.load_setuptools_entrypoints("devgraph.discovery.molecules")
+        self.pm = PluginManager("devgraph.molecules")
+        self.pm.load_setuptools_entrypoints("devgraph.molecules")
         self.providers = dict([(self.pm.get_name(i), i) for i in self.pm.get_plugins()])
         logger.debug("Loaded discovery molecules: " + ", ".join(self.providers.keys()))
 
